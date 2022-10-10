5e307bf3c25241c58f941f3baf7bc345cec046a6
Adrian Rocke <me@adrianrocke.com>
2022-10-09 23:07:17 -0500

    feat: update a reference

--- libs/domain/zettel/src/Reference.spec.ts
+++ libs/domain/zettel/src/Reference.spec.ts
@@ -3,16 +3,56 @@ import Reference from './Reference'
 describe('create', () => {
   test('creates a new Reference with a random id', () => {
     const name = 'new reference'
+    const creators = ['Adam', 'Eve']
     const reference = Reference.create({
       name,
+      creators,
     })
     expect(reference.name).toEqual(name)
-    expect(
-      reference.equals(
-        Reference.create({
-          name,
-        })
-      )
-    ).toEqual(false)
+    expect(reference.creators).toEqual(creators)
+    expect(reference.equals(Reference.create({ name, creators }))).toEqual(
+      false
+    )
+  })
+})
+
+describe('update', () => {
+  test('updates all properties on the Reference', () => {
+    const reference = Reference.create({
+      name: 'old name',
+      creators: ['old creator'],
+    })
+    const name = 'new reference'
+    const creators = ['Adam', 'Eve']
+    const url = 'https://google.com'
+    reference.update({ name, creators, url })
+    expect(reference.name).toEqual(name)
+    expect(reference.creators).toEqual(creators)
+    expect(reference.url).toEqual(url)
+  })
+
+  test('does nothing with undefined', () => {
+    const name = 'new reference'
+    const creators = ['Adam', 'Eve']
+    const url = 'https://google.com'
+    const reference = Reference.create({ name, creators, url })
+    reference.update({})
+    expect(reference.name).toEqual(name)
+    expect(reference.creators).toEqual(creators)
+    expect(reference.url).toEqual(url)
+  })
+
+  test('unsets with undefined', () => {
+    const name = 'new reference'
+    const creators = ['Adam', 'Eve']
+    const reference = Reference.create({
+      name,
+      creators,
+      url: 'https://google.com',
+    })
+    reference.update({ url: null })
+    expect(reference.name).toEqual(name)
+    expect(reference.creators).toEqual(creators)
+    expect(reference.url).toEqual(undefined)
   })
 })
--- libs/domain/zettel/src/Reference.ts
+++ libs/domain/zettel/src/Reference.ts
@@ -2,16 +2,56 @@ import { Entity, Identifier } from '@zettelmaster/domain/base'
 
 export interface ReferenceData {
   name: string
+  creators: string[]
+  url?: string
 }
 
-/** Represents a reference used in zettel notes. */
+export type UpdateReferenceData = {
+  name?: string
+  creators?: string[]
+  url?: string | null
+}
+
+/** Represents a resource referenced in zettel notes. */
 export default class Reference extends Entity<ReferenceData> {
+  /** The name of the resource. */
   get name() {
     return this._data.name
+  }
+
+  /** The list of creators of the resource. */
+  get creators(): readonly string[] {
+    return this._data.creators
+  }
+
+  /** The URL to access the resource with. */
+  get url(): string | undefined {
+    return this._data.url
   }
 
   /** Creates a new Reference. */
   static create(data: ReferenceData): Reference {
     return new Reference(Identifier.generate(), data)
+  }
+
+  /**
+   * Partial update of fields on the Reference.
+   * Passing undefined will ignore that property,
+   * while passing null will remove that property.
+   */
+  update(data: Partial<UpdateReferenceData>) {
+    if (typeof data.name !== 'undefined') {
+      this._data.name = data.name
+    }
+    if (typeof data.creators !== 'undefined') {
+      this._data.creators = data.creators
+    }
+    if (typeof data.url !== 'undefined') {
+      if (data.url === null) {
+        delete this._data.url
+      } else {
+        this._data.url = data.url
+      }
+    }
   }
 }
